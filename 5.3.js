cb69027d5f8b61a56b877b16543d8773a31a5af3
Vadim <www.vadim.2@mail.ru>
2022-04-07 21:04:39 +0300

    0.1

---
+++ 5.3.js
@@ -0,0 +1,8 @@
+//5.3.1//Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом.
+//let a = prompt('Введите имя');
+//let ucFirst =() => {
+//    a = a[0].toUpperCase() + a.slice(1);
+//    return a;
+//}
+//ucFirst()
+//alert(a)
